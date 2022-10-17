28d2bc78c6fd570adef21068ecdbfe9729bd5cda
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-17 14:19:52 +0900

    [Add] Shipping

--- shipping/index.js
+++ shipping/index.js
@@ -3,19 +3,22 @@ const compose =
 	(arg) =>
 		fns.reduce((composed, fn) => fn(composed), arg);
 
-let shopping_cart = [];
-
 const addItem = (item) => (cart) => [...cart, { name: item.name, price: item.price }];
 const calc_total = (cart) => cart.reduce((sum, item) => sum + item.price, 0);
 const calc_tax = (cart) => calc_total(cart) * 0.1;
 const get_total_price = (cart) => calc_total(cart) + calc_tax(cart);
-const is_free_shipping = (total) => total > 4000;
+const condition = () => 4000;
+const is_free_shipping = (total) => total > condition();
+
+const send_email = (email) => (free) =>
+	free ? `To ${email} -  Free shipping is available` : `To ${email} - Free shipping is NOT available.`;
 
 const ret = compose(
 	addItem({ name: 'tissue', price: 1000 }),
 	addItem({ name: 'box', price: 500 }),
 	addItem({ name: 'nail', price: 2300 }),
 	get_total_price,
-	is_free_shipping
-)(shopping_cart);
+	is_free_shipping,
+	send_email('rov@gmail.com')
+)([]);
 console.log(ret);
